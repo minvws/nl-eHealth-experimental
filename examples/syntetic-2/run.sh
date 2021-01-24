0619bdf658420abc32bd46a2aa7da2d38352af29
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-01-24 22:17:59 +0100

    Clean up run file - so we can do more files.

--- examples/syntetic-2/run.sh
+++ examples/syntetic-2/run.sh
@@ -6,23 +6,25 @@ PYTHON=${PYTHON:-/opt/local/bin/python2.7}
 test -d pbjson || git clone  git clone git@github.com:yinqiwen/pbjson.git
 test -d protobuf-json || git@github.com:dpp-name/protobuf-json.git
 
-${PYTHON} pbjson/schema_parse.py "Vaccination-FHIR-Bundle - GC.json" >  "Vaccination-FHIR-Bundle - GC.proto" 
-
-protoc -I=. --python_out=. ./"Vaccination-FHIR-Bundle - GC.proto"
-
-cp "Vaccination_FHIR_Bundle _ GC_pb2.py" msg_pb2.py
-
-PYTHONPATH=.:protobuf-json /opt/local/bin/python2.7 ./json2pb.py  "Vaccination-FHIR-Bundle - GC.json"  >  "Vaccination-FHIR-Bundle - GC.pb"
-
-echo "file	| 	plain	| 	compressed"
-echo "----------|---------------|-----------------"
-
-for i in pb json xml
+echo "file	                        | plain	   | compressed"
+echo "----------------------------------|----------|-----------------"
+for i in *.json
 do
-	for j in *.$i
+	rm -f msg*
+
+	j=$(basename "$i" .json)
+	(
+		echo 'syntax = "proto2";'
+		${PYTHON} pbjson/schema_parse.py "$i" 
+	)> msg.proto
+        protoc -I=. --python_out=. msg.proto
+
+        PYTHONPATH=.:protobuf-json /opt/local/bin/python2.7 ./json2pb.py  "$i" > "$j.pb"
+
+	for ex in pb json xml
 	do
-		PLAIN=$(cat "$j" | wc -c)
-		COMP=$(cat "$j" | xz --compress  -e | wc -c)
-		echo "$j	| $PLAIN |	$COMP"
+		PLAIN=$(cat "$j.$ex" | wc -c)
+		COMP=$(cat "$j.$ex" | xz --compress  -e | wc -c)
+		echo "$j / $ex	| $PLAIN |	$COMP"
 	done
 done
