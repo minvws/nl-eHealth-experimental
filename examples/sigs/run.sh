a47bd5bd4ba35bd9d78bcedbeca12a4c2b5f1813
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-01-25 12:12:16 +0100

    Also create QR; use real protobuff

--- examples/sigs/run.sh
+++ examples/sigs/run.sh
@@ -5,12 +5,10 @@ set -e
 openssl ecparam -name secp256k1 -genkey -noout -out ec.key
 openssl ec -in ec.key -pubout -out ec.pub
 
-echo Something to Sign > payload
-
 # Sign something
 #
-openssl dgst -sha256 -sign ec.key payload > signature.bin
-openssl dgst -sha256 -verify ec.pub -signature signature.bin payload
+openssl dgst -sha256 -sign ec.key payload.pb > signature.bin
+openssl dgst -sha256 -verify ec.pub -signature signature.bin payload.pb
 
 echo EC256 k1
 echo pubkey: 
@@ -18,10 +16,9 @@ cat ec.pub
 echo Sginature:
 cat signature.bin | base64
 
-
 openssl req -new -x509 -nodes -keyout x509.key -out x509.pub -subj /CN=CoronaPub -set_serial 81238912371298631278
 
-openssl cms -nosmimecap -nocerts -noattr -sign -signer x509.pub -inkey x509.key -outform DER -out signature-cms.bin -in payload
+openssl cms -binary -nosmimecap -nocerts -noattr -sign -signer x509.pub -inkey x509.key -outform DER -out signature-cms.bin -in payload.pb
 
 echo
 echo X509
@@ -30,7 +27,8 @@ echo
 echo Signature using above cert:
 cat signature-cms.bin | base64
 
-# zip fake.zip payload signature.bin signature-cms.bin
+# openssl asn1parse -inform DER -in signature-cms.bin 
 
-openssl asn1parse -inform DER -in signature-cms.bin 
+cat signature-cms.bin payload.pb | xz -e --compress > qr.bin
+cat qr.bin | qrencode -o qr.png -8 -l H
 
