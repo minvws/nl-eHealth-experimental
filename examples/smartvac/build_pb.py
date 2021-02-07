4c198c259ca12967ce97e5485c8069bfa0008bb4
Aleksander Okonski <aleksander.oko@attackpointsecurity.com>
2021-02-07 16:37:59 +0100

    Added simple mutli-level disclosure to repo

--- examples/smartvac/build_pb.py
+++ examples/smartvac/build_pb.py
@@ -10,6 +10,8 @@ The protobuf object (NNN_pb2.py) is created by using:
 import json
 import protobuf_json
 import smart_vacc_fhir_bundle_gc_pb2
+import smart_vacc_fhir_bundle_gc1_pb2
+import smart_vacc_fhir_bundle_gc2_pb2
 import sys
 
 from pathlib import Path
@@ -18,6 +20,30 @@ from pathlib import Path
 def serialize(json_source: Path) -> None:
     svc_pb2 = smart_vacc_fhir_bundle_gc_pb2.SmartVaccCert()
     out_file = json_source.with_suffix(".bin")
+
+    # populate protobuf object from json
+    with open(json_source, "r") as f:
+        svc_str = json.loads(f.read())
+        pb2 = protobuf_json.json2pb(svc_pb2, svc_str)
+    # write out serialized protobuf
+    with open(out_file, "wb") as f:
+        f.write(pb2.SerializeToString())
+
+    ## LEVEL1
+    svc_pb2 = smart_vacc_fhir_bundle_gc1_pb2.SmartVaccCert1()
+    out_file = json_source.with_suffix(".1.bin")
+
+    # populate protobuf object from json
+    with open(json_source, "r") as f:
+        svc_str = json.loads(f.read())
+        pb2 = protobuf_json.json2pb(svc_pb2, svc_str)
+    # write out serialized protobuf
+    with open(out_file, "wb") as f:
+        f.write(pb2.SerializeToString())
+    
+    ## LEVEL2
+    svc_pb2 = smart_vacc_fhir_bundle_gc2_pb2.SmartVaccCert2()
+    out_file = json_source.with_suffix(".2.bin")
 
     # populate protobuf object from json
     with open(json_source, "r") as f:
