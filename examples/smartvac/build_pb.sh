a7a36bb4769d40755403316da09a916fac1f6b3b
Gaby Whitehead <76425117+gabywh@users.noreply.github.com>
2021-02-08 20:49:45 +0100

    build_pb.sh supports multi-level disclosure

--- examples/smartvac/build_pb.sh
+++ examples/smartvac/build_pb.sh
@@ -6,12 +6,16 @@ PYTHON_BIN=$(which python3)
 PYTHON_SCRIPT=build_pb.py
 JSON_SOURCE="Vaccination-FHIR-Bundle - GC.json"
 PB_FROM_JSON=$(basename "${JSON_SOURCE}" .json)
-PB_FROM_JSON="${PB_FROM_JSON}.bin"
 
 PYTHONPATH=.:protobuf-json "${PYTHON_BIN}" "${PYTHON_SCRIPT}" "${JSON_SOURCE}"
 
 # some simple size stats
 wc -c "${JSON_SOURCE}"
-wc -c "${PB_FROM_JSON}"
-${COMPRESSOR} ${COMPRESS_OPTS} "${PB_FROM_JSON}"
-wc -c "${PB_FROM_JSON}.xz"
+
+for disclosure_level in {0..2};
+do
+  pb_json="${PB_FROM_JSON}.${disclosure_level}.bin"
+  wc -c "${pb_json}"
+  ${COMPRESSOR} ${COMPRESS_OPTS} "${pb_json}"
+  wc -c "${pb_json}.xz"
+done
