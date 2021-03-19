f0bdfad3c586745be58597d9fc3de9b27d5813da
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-03-19 11:23:49 +0100

    Example with various encodings.

---
+++ examples/b45/README.md
@@ -0,0 +1,33 @@
+```
+plain/8		L	399 x 399 	159201 cells
+b45/8		L	149 x 149 	22201 cells
+b45/2		L	125 x 125 	15625 cells
+zl/8		L	101 x 101 	10201 cells
+zl/b45/2	L	101 x 101 	10201 cells
+C/zl/8		L	101 x 101 	10201 cells
+C/zl/b45/2	L	101 x 101 	10201 cells
+
+plain/8		M	447 x 447 	199809 cells
+b45/8		M	169 x 169 	28561 cells
+b45/2		M	141 x 141 	19881 cells
+zl/8		M	117 x 117 	13689 cells
+zl/b45/2	M	117 x 117 	13689 cells
+C/zl/8		M	113 x 113 	12769 cells
+C/zl/b45/2	M	113 x 113 	12769 cells
+
+plain/8		H	FAIL
+b45/8		H	FAIL
+b45/2		H	FAIL
+zl/8		H	153 x 153 	23409 cells
+zl/b45/2	H	153 x 153 	23409 cells
+C/zl/8		H	149 x 149 	22201 cells
+C/zl/b45/2	H	149 x 149 	22201 cells
+
+plain/8		Q	519 x 519 	269361 cells
+b45/8		Q	FAIL
+b45/2		Q	165 x 165 	27225 cells
+zl/8		Q	137 x 137 	18769 cells
+zl/b45/2	Q	137 x 137 	18769 cells
+C/zl/8		Q	133 x 133 	17689 cells
+C/zl/b45/2	Q	133 x 133 	17689 cells
+
---
+++ examples/b45/gen.sh
@@ -0,0 +1,37 @@
+QRFLAGS="-m 0 -s 1  "
+set -e
+
+gs() {
+F=`file x.png | sed -e 's/.*data, //' -e 's/,.*//'`
+set $F
+L=`expr $1 \* $3`
+echo "$F 	$L cells"
+}
+
+for E in L M H Q
+do
+rm -f x.png
+/bin/echo -n "plain/8		$E	"
+cat appendix-h.json| qrencode -8 -l $E -o x.png 2>/dev/null &&  gs  || echo FAIL
+
+/bin/echo -n "b45/8		$E	"
+cat appendix-h.json| ~/go/bin/qrbase45tool  -i /dev/stdin -o /dev/stdout | qrencode  -8 ${QRFLAGS}  --l $E -o x.png 2>/dev/null && gs || echo FAIL
+
+/bin/echo -n "b45/2		$E	"
+cat appendix-h.json| ~/go/bin/qrbase45tool  -i /dev/stdin -o /dev/stdout | qrencode  ${QRFLAGS} -v 2 -l $E -o x.png 2>/dev/null && gs || echo FAIL
+
+/bin/echo -n "zl/8		$E	"
+cat appendix-h.json| openssl zlib | qrencode ${QRFLAGS} -8 -l $E -o x.png; gs
+
+/bin/echo -n "zl/b45/2	$E	"
+cat appendix-h.json| openssl zlib | ~/go/bin/qrbase45tool  -i /dev/stdin -o /dev/stdout | qrencode ${QRFLAGS}  -v2 -l $E -o x.png; gs
+
+/bin/echo -n "C/zl/8		$E	"
+cat appendix-h.json| json2cbor | openssl zlib | qrencode ${QRFLAGS} -8 -l $E -o x.png; gs
+
+/bin/echo -n "C/zl/b45/2	$E	"
+cat appendix-h.json| json2cbor | openssl zlib | ~/go/bin/qrbase45tool  -i /dev/stdin -o /dev/stdout | qrencode ${QRFLAGS}  -v2 -l $E -o x.png; gs
+
+echo
+done
+
