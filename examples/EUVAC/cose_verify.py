9d787292667f18cd776f70d052f419d382da6b74
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-03-25 20:23:34 +0100

    Allow skipping of signature check

--- examples/EUVAC/cose_verify.py
+++ examples/EUVAC/cose_verify.py
@@ -27,27 +27,27 @@ b45in = sys.stdin.read()
 cin = b45decode(b45in)
 encoded = zlib.decompress(cin)
 
-with open('dsc-worker.pem','rb') as file:
-  pem = file.read()
-cert = x509.load_pem_x509_certificate(pem)
-pub = cert.public_key().public_bytes(encoding=serialization.Encoding.Raw,format=serialization.PublicFormat.Raw)
-fingerprint = cert.fingerprint(hashes.SHA256())
-keyid = fingerprint[-8:]
-
 decoded = CoseMessage.decode(encoded)
 
-if (decoded.phdr[KID] != keyid):
-  raise Exception('KeyID is unknown (not mine) -- cannot verify.')
+if len(sys.argv) != 2 or sys.argv[1] != '--ignore-signature':
+   with open('dsc-worker.pem','rb') as file:
+       pem = file.read()
+   cert = x509.load_pem_x509_certificate(pem)
+   pub = cert.public_key().public_bytes(encoding=serialization.Encoding.Raw,format=serialization.PublicFormat.Raw)
+   fingerprint = cert.fingerprint(hashes.SHA256())
+   keyid = fingerprint[-8:]
 
-decoded.key = CoseKey.from_dict({
+   if (decoded.phdr[KID] != keyid):
+    raise Exception('KeyID is unknown (not mine) -- cannot verify.')
+
+   decoded.key = CoseKey.from_dict({
         KpKty: KtyOKP,
         OKPKpCurve: Ed25519, 
         KpKeyOps: [SignOp, VerifyOp],
         OKPKpX: pub,
-})
+   })
 
-
-if (not decoded.verify_signature()) :
-  raise Exception('faulty sig')
+   if (not decoded.verify_signature()) :
+      raise Exception('faulty sig')
 
 print(decoded.payload)
