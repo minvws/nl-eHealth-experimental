39abf29681e947ef1aace40af48adad22e514186
Gaby Whitehead <76425117+gabywh@users.noreply.github.com>
2021-03-26 11:00:04 +0100

    Flask webserver

--- examples/EUVAC/fhir2qr.py
+++ examples/EUVAC/fhir2qr.py
@@ -1,40 +1,37 @@
 from fhir_query import FhirQueryImmunization
+from flask import Flask, request, url_for
 from immu_parser import ImmuEntryParser
-from min_data_set import MinDataSet, MinDataSetFactory
-from pprint import PrettyPrinter
 
 
-def __extract_entry(qry_entry: dict):
-    # General approach to processing the JSON: we just skip any JSON items that are not of interest to us
-    # We are looking for resourceType of Immunization but we tread carefully, checking for existence
-    # of keys at each step for two reasons:
-    # 1. we can quickly skip over any entries that are not of interest - the sooner the better, and
-    # 2. we don't want any JSON that we are not interested in to disturb our processing by raising
-    # an InvalidKey exception which would be the case if we didn't explicitly check keys at each level
-    # in the JSON hierarchy
-    if "resource" in qry_entry:
-        resource: dict = qry_entry["resource"]
-        if "resourceType" in resource:
-            if resource["resourceType"] == "Immunization":
-                patient = FhirQueryImmunization.resolve_patient(resource=resource)
-                min_data_set: MinDataSet = ImmuEntryParser.get_min_data_set(
-                    resource=resource,
-                    patient=patient,
-                    disclosure_level=MinDataSetFactory.DisclosureLevel.MD,
-                )
-                print("min_data_set.pv/bc:")
-                pp.pprint(min_data_set.pv)
-                print("min_data_set.md:")
-                pp.pprint(min_data_set.md)
+class Fhir2QR:
+    def __init__(self, fhir_server: str):
+        self.__fhir_server: str = fhir_server
+
+    def fhir_query_immu(self) -> dict:
+        qry_res = FhirQueryImmunization.find()
+        return qry_res
 
 
-if __name__ == "__main__":
-    pp = PrettyPrinter(indent=2)
-    qry_res = FhirQueryImmunization.find()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return url_for("static", filename="index.html")
+
+
+@app.route("/fhir2qr", methods=["POST"])
+def fhir2rr(fhir_server: str):
+    fhir2qr_query = Fhir2QR(fhir_server=fhir_server)
+    qry_res: dict = fhir2qr_query.fhir_query_immu()
+    ret_data: dict = {}
+
     if "entry" in qry_res:
         for entry in qry_res["entry"]:
-            __extract_entry(qry_entry=entry)
+            parsed_entry: dict = ImmuEntryParser.extract_entry(qry_entry=entry)
+            ret_data.update(parsed_entry)
     if "resourceType" in qry_res:
         if qry_res["resourceType"] == "Bundle":
             total_matches = qry_res["total"]
-            print(f"\n ** Total FHIR query matches found: {total_matches} **")
+            ret_data["Total Matches"] = total_matches
+    return ret_data
